4a0602a6ac310a235347f0d3d70d4fc1242fda21
XCFQ909 <chrisnam03@naver.com>
2023-07-21 14:33:44 +0900

    clock

---
+++ js/clock.js
@@ -0,0 +1,39 @@
+const theTime = document.querySelector('#theTime');
+const theSecond = document.querySelector('#seconds');
+const theDay = document.querySelector("#whatDay");
+
+function getTime() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    theTime.innerText = `${hours} : ${minutes}`;
+}
+
+function getSecond() {
+    const date = new Date();
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    theSecond.innerText = seconds;
+}
+
+function getDay() {
+    const date = new Date();
+    const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    const dayOfWeek = week[date.getDay() - 1];
+    console.log(dayOfWeek);
+    
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+    const whatMonth = months[date.getMonth()];
+    console.log(whatMonth);
+
+    const dayOfMonth = String(date.getDate());
+    console.log(dayOfMonth);
+
+    theDay.innerText = `${whatMonth} ${dayOfMonth}, ${dayOfWeek}`
+}
+
+getTime();
+getSecond();
+getDay();
+setInterval(getTime, 1000);
+setInterval(getSecond, 1000);
+setInterval(getDay, 1000);
